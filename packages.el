64faa185472426e54f609e6d44957bd8bec8d92d
Thrilok emmadisetty <63156546+Thrilok28021996@users.noreply.github.com>
2025-07-10 14:53:45 +0530

    Create packages.el

---
+++ packages.el
@@ -0,0 +1,39 @@
+;; -*- no-byte-compile: t; -*-
+;;; packages.el --- EXWM packages for Doom Emacs
+
+;; EXWM and related packages
+(package! exwm)
+(package! desktop-environment)
+
+;; Audio control
+(package! pulseaudio-control)
+
+;; System monitoring
+(package! symon)
+
+;; Application launcher
+(package! counsel)
+
+;; Window management enhancements
+(package! switch-window)
+
+;; Notification support
+(package! alert)
+
+;; Terminal emulator (if not using external terminal)
+(package! vterm)
+
+;; File manager integration
+(package! dired-sidebar)
+
+;; PDF viewer
+(package! pdf-tools)
+
+;; Media player control
+(package! emms)
+
+;; System information
+(package! fancy-battery)
+
+;; Time and date
+(package! time)
